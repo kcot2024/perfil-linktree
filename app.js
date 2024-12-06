2e3ede7a3aec596899d5aae3c2d8f883aaf0b655
kcot2024 <kcotrina_m@hotmail.com>
2024-12-06 11:06:57 -0500

    Se hizo projecto perfil tree

---
+++ app.js
@@ -0,0 +1,13 @@
+const contenedor = document.getElementById("userContainer");
+const nombre = document.createElement("h2");
+nombre.textContent ="Kener Cotrina";
+contenedor.appendChild(nombre);
+nombre.style.color = "#ffff"
+nombre.style.marginBottom = "15px";
+
+const descripcion = document.createElement("h2");
+descripcion.textContent = "¡Hola! Para saber más sobre mí, sígueme en mis redes"
+userContainer.appendChild(descripcion)
+descripcion.style.color = "#ffff"
+descripcion.style.fontSize = "14px"
+descripcion.style.marginBottom = "15px";
